19ee298331f66887952e0859cf619e9f332bdce1
ajo <ajoalex012@gmail.com>
2021-07-12 20:27:14 +0530

    desktop ui completed

---
+++ src/Components/Cta/Cta.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Cta.css'
+function Cta() {
+    return (
+        <div className="cta">
+           <div className="container text-center">
+                <h2>Boost your links today</h2>
+                <button>Get Started</button>
+           </div>
+        </div>
+    )
+}
+
+export default Cta
